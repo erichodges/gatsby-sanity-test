39d6bf771a7dcd14e26ee448495117f9b7110886
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-04-25 13:22:47 -0700

    Added Sanity Studio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -12,9 +16,15 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "0ktclwli",
+        dataset: "dev",
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.MY_SANITY_TOKEN,
+      },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
